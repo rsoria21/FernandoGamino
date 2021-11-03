02a64626ba95dc33a3abbb2dd435a3747ed2de49
rsoria21 <69863317+rsoria21@users.noreply.github.com>
2021-11-03 13:26:37 -0500

    new form added

---
+++ contact/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+  $(".submit").click(function (event) {
+    console.log("Clicked button");
+
+    var email = $(".email").val();
+    var subject = $(".subject").val();
+    var message = $(".message").val();
+    var statusElm = $(".status");
+    statusElm.empty();
+
+    if (email.length > 5 && email.includes("@") && email.includes(".")) {
+      statusElm.append("<div>Email is valid </div>");
+    } else {
+      event.preventDefault();
+      statusElm.append("<div>Email is not valid </div>");
+    }
+
+    if (subject.length >= 2) {
+      statusElm.append("<div>Subject is valid </div>");
+    } else {
+      event.preventDefault();
+      statusElm.append("<div>Subject is not valid </div>");
+    }
+
+    if (message.length >= 10) {
+      statusElm.append("<div>Message is valid </div>");
+    } else {
+      event.preventDefault();
+      statusElm.append("<div>Message is not valid </div>");
+    }
+  });
+});
+
+// START NAV
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+// END NAV
