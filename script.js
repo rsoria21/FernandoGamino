fe3d8234eaf4fd6de6f29a01ff1b8d3c5718b5b9
rsoria21 <69863317+rsoria21@users.noreply.github.com>
2021-11-03 13:24:16 -0500

    adjusted JS

--- script.js
+++ script.js
@@ -1,8 +1,46 @@
-// START NAV
-function openNav() {
+// start nav
+openNav = () => {
   document.getElementById("mySidenav").style.width = "250px";
-}
-function closeNav() {
+};
+
+closeNav = () => {
   document.getElementById("mySidenav").style.width = "0";
+};
+
+// End Nav
+
+// Slidshow
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
 }
-// END NAV
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+// end nav
